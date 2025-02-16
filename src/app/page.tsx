a28d09c7100293cccd737e62b9571b6ba7182276
Tay <buley@outlook.com>
2025-02-16 14:59:56 -0800

    vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,8 @@
-"use client";
-import React from "react";
-import Image from "next/image";
-import { useEffect } from 'react';
+import React from 'react';
+import AppLayout from '@/components/AppLayout';
 
 export default function Home() {
-  
-  const triangle = <Image
-    src="/vercel.svg"
-    alt="Vercel Logo"
-    width={72}
-    height={16}
-  />
-
   return (
-    <div>
-      {triangle}
-      <h1>Hello, World!</h1>
-    </div>
+    <AppLayout />
   );
 }
